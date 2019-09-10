dcfecc42185b0ecd67e68f7ab32cea1756b34456
kainthsourav <souravkainth@outlook.com>
2019-09-10 16:43:54 +0530

    Sign Up Function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username:any;
+  password:any;
+  
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  SignUp()
+  {
+    this.router.navigate(['/signup']);
   }
 
 }
