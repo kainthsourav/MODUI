a8ad7555d47f5bc79080e41c2ac868e428d30fef
Kainth Sourav <35909895+kainthsourav@users.noreply.github.com>
2019-09-26 22:29:38 +0530

    Ng Added

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+ Username:string;
+ Email:string;
+ Password:string;
+ CPassword:string;
+ Fname:string;
+ Lname:string;
+ Mobile:string;
+ 
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+   Signup()
+  {
+    if(this.Username!=undefined && this.Email!=undefined
+      && this.Password!=undefined && this.CPassword!=undefined
+      && this.Fname!=undefined && this.Lname!=undefined && this.Mobile!=undefined)
+      {
+          if(this.Password==this.CPassword)
+          {
+        console.log(this.Username);
+        console.log(this.Email);
+        console.log(this.Password);
+        console.log(this.CPassword);
+        console.log(this.Fname);
+        console.log(this.Lname);
+        console.log(this.Mobile);
+          }
+       
+      }
+  }
 }
--- src/app/trainer-sign-up/trainer-sign-up.component.ts
+++ src/app/trainer-sign-up/trainer-sign-up.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerSignUpComponent implements OnInit {
 
+  Username:any;
+  Email:any;
+  Password:any;
+  Cpassword:any;
+  Fname:any;
+  Lname:any;
+  Mobile:any;
+  Timezone:any;
+  Timing:any;
+  OtherTech:any;
+  Facility:any;
+  Linkedin:any;
+  Exp:any;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+Tsignup()
+{
+  console.log(this.Username);
+  console.log(this.Password);
+  console.log(this.Cpassword);
+  console.log(this.Fname);
+  console.log(this.Lname);
+  console.log(this.Mobile);
+  console.log(this.Timezone);
+  console.log(this.Timing);
+  console.log(this.OtherTech);
+  console.log(this.Facility);
+  console.log(this.Linkedin);
+  console.log(this.Exp)
+}
 
 }
