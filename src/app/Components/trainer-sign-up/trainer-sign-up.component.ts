e6644093f8ed416e56fcd54c848d15be6510599f
Kainth Sourav <35909895+kainthsourav@users.noreply.github.com>
2019-10-01 01:12:53 +0530

    Mentor Sign Up  Form --Validation --Half Comp

--- src/app/Components/trainer-sign-up/trainer-sign-up.component.ts
+++ src/app/Components/trainer-sign-up/trainer-sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-trainer-sign-up',
@@ -7,39 +8,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerSignUpComponent implements OnInit {
 
-  Username:any;
-  Email:any;
-  Password:any;
-  Cpassword:any;
-  Fname:any;
-  Lname:any;
-  Mobile:any;
-  Timezone:any;
-  Timing:any;
-  OtherTech:any;
-  Facility:any;
-  Linkedin:any;
-  Exp:any;
+  // Username:any;
+  // Email:any;
+  // Password:any;
+  // Cpassword:any;
+  // Fname:any;
+  // Lname:any;
+  // Mobile:any;
+  // Timezone:any;
+  // Timing:any;
+  // OtherTech:any;
+  // Facility:any;
+  // Linkedin:any;
+  // Exp:any;
 
-  constructor() { }
+  myForm:FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.myForm=fb.group({
+      Username:['', [Validators.required]],
+      Email:['', [Validators.required]],
+      Password:['', [Validators.required]],
+      Cpassword:['', [Validators.required]],
+      Fname:['', [Validators.required]],
+      Lname:['', [Validators.required]],
+      Mobile:['', [Validators.required]],
+      Timezone:['', [Validators.required]],
+      Timing:['', [Validators.required]],
+      OtherTech:['', [Validators.required]],
+      Facility:['', [Validators.required]],
+      Linkedin:['', [Validators.required]],
+      Exp:['', [Validators.required]],
+   });
+   }
 
   ngOnInit() {
   }
 
-Tsignup()
+// Tsignup()
+// {
+//   console.log(this.Username);
+//   console.log(this.Password);
+//   console.log(this.Cpassword);
+//   console.log(this.Fname);
+//   console.log(this.Lname);
+//   console.log(this.Mobile);
+//   console.log(this.Timezone);
+//   console.log(this.Timing);
+//   console.log(this.OtherTech);
+//   console.log(this.Facility);
+//   console.log(this.Linkedin);
+//   console.log(this.Exp)
+// }
+
+MentorSignup(myForm)
 {
-  console.log(this.Username);
-  console.log(this.Password);
-  console.log(this.Cpassword);
-  console.log(this.Fname);
-  console.log(this.Lname);
-  console.log(this.Mobile);
-  console.log(this.Timezone);
-  console.log(this.Timing);
-  console.log(this.OtherTech);
-  console.log(this.Facility);
-  console.log(this.Linkedin);
-  console.log(this.Exp)
+  
+    console.log(myForm.value);
+  
 }
+
 
 }
