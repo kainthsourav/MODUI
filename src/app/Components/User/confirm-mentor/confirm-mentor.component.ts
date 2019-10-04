e6fea37cb3eee10c65f52b1e72619d6a9aba9719
kainthsourav <souravkainth@outlook.com>
2019-10-04 17:56:16 +0530

    Confirm Mentor --Trying

---
+++ src/app/Components/User/confirm-mentor/confirm-mentor.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import{GetUsersService} from '../../../Services/get-users.service';
+
+
+@Component({
+  selector: 'app-confirm-mentor',
+  templateUrl: './confirm-mentor.component.html',
+  styleUrls: ['./confirm-mentor.component.css']
+})
+export class ConfirmMentorComponent implements OnInit {
+
+  QueId;
+  QueTech;
+  MyData;
+  constructor(private route: ActivatedRoute,private Service:GetUsersService) {
+    this.route.queryParams.subscribe(params => {
+      this.QueId=params['ID'];
+      console.log(this.QueId);
+    });
+   
+       this.Service.GetUserById(this.QueId).subscribe(data=>{
+        this.MyData=data;
+        console.log(this.MyData);
+      });
+   }
+
+  ngOnInit() {
+  }
+
+  
+  
+
+}
