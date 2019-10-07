9d1e43be9b1e66efd708d6a5825385b2001737eb
Kainth Sourav <35909895+kainthsourav@users.noreply.github.com>
2019-10-07 15:37:43 +0530

    Current Training -Traininer

--- src/app/Components/Mentor/trainer-menu/trainer-menu.component.ts
+++ src/app/Components/Mentor/trainer-menu/trainer-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {GetUsersService} from '../../../Services/get-users.service';
+import * as _ from "underscore";
+
 
 @Component({
   selector: 'app-trainer-menu',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trainer-menu.component.css']
 })
 export class TrainerMenuComponent implements OnInit {
-
-  constructor() { }
+  myTrainings;
+  Data;
+  constructor(private myService:GetUsersService) { }
 
   ngOnInit() {
+    this.getTrainings();
   }
 
+
+  getTrainings()
+  {
+    this.myService.trainingApprovals().subscribe(data=>{
+      this.myTrainings=_.where(data,{accept:true,mentorId:2,PaymentStatus:true});
+      console.log(this.myTrainings);
+     console.log(Object.keys(this.myTrainings).length);
+     if(Object.keys(this.myTrainings).length>0)
+     {
+       this.Data=false;
+     }
+     else
+     {
+       this.Data=true;
+     }
+    });
+  }
 }
